3369c3f2c36993a81a92291b5cdfb27e2d031645
APindev <91620937+AtharvaaPlayz@users.noreply.github.com>
2022-08-09 11:44:41 +0530

    Add files via upload

---
+++ Athon.py
@@ -0,0 +1,45 @@
+#Athon
+
+import pickle as oorka
+import os as opsys
+
+def display(a):
+    print(a)
+
+#ex display("hi")
+
+#----------------------------------------------------------------------------------#
+
+def plus(a, b):
+    ans = a + b
+    print(ans)
+    
+#ex plus(1, 2)
+
+#----------------------------------------------------------------------------------#
+
+def minus(a, b):
+    ans = a - b
+    print(ans)
+    
+#ex minus(6, 5) 5, 6 = negative number
+
+#--------------------------------------------------------------------------------#
+
+def multiply(a, b):
+    ans = a * b
+    print(ans)
+    
+#ex multiply(5, 6)
+
+#------------------------------------------------------------------------------#
+
+def divide(a, b):
+    ans = a / b
+    print(ans)
+
+#Thanks to: http://stackoverflow.com/ | Creators OF Plugin Pickle | Creators of Plugin os | Very Thanks Without You This Will Not Be Possible! |
+
+#Was Created By Atharvaa Velliyangiri 
+#Athon OR A
+
